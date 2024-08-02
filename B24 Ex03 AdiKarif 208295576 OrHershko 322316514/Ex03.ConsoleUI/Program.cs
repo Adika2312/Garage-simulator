0aa0aa05f9ac58147ba9600e85b6fb7977e6c5a1
Adi Karif <adika3@mta.ac.il>
2024-08-02 18:21:40 +0300

    Final project

---
+++ B24 Ex03 AdiKarif 208295576 OrHershko 322316514/Ex03.ConsoleUI/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Ex03.ConsoleUI
+{
+    // $G$ DSN-001 (-5) Missing base energy provider class with concrete Electric/Gas derived classes.
+    internal class Program
+    {
+        public static void Main()
+        {
+            // $G$ CSS-999 (-3) Missing blank line after local variable
+            UserInterface userInterface = new UserInterface();
+            userInterface.RunGarage();
+        }
+    }
+}
---
+++ B24 Ex03 AdiKarif 208295576 OrHershko 322316514/Ex03.GarageLogic/Wheel.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Ex03.GarageLogic
+{
+    public class Wheel
+    {
+        // $G$ DSN-999 (-3) This kind of field should be readonly.
+        private string m_ManufacturerName;
+        private float m_CurrentAirPressure;
+        // $G$ DSN-999 (-3) This kind of field should be readonly.
+        private float m_MaxAirPressure;
+
+        public string ManufacturerName
+        {
+            get { return m_ManufacturerName;}
+            set { m_ManufacturerName = value; }
+        }
+
+        public float CurrentAirPressure
+        {
+            get { return m_CurrentAirPressure; }
+            set
+            {
+                if (value >= 0 && value <= MaxAirPressure)
+                {
+                    m_CurrentAirPressure = value;
+                }
+                else
+                {
+                    throw new ValueOutOfRangeException(0, MaxAirPressure);
+                }
+            }
+        }
+
+        public float MaxAirPressure
+        {
+            get { return m_MaxAirPressure; }
+            set { m_MaxAirPressure = value; }
+        }
+        public void Inflate(float i_AirToAdd)
+        {
+            while(true)
+            {
+                if (i_AirToAdd + m_CurrentAirPressure > m_MaxAirPressure)
+                {
+                    throw new ValueOutOfRangeException(0, m_MaxAirPressure - m_CurrentAirPressure);
+                }
+
+                m_CurrentAirPressure += i_AirToAdd;
+                break;
+            }
+        }
+    }
+}
